48c5cdc8a1edee20270ea032a95970e82b8422b0
faizazum17 <18220059@std.stei.itb.ac.id>
2022-09-27 21:42:47 +0700

    update API

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = FastAPI()
 students = [
 	{'NIM': 18220059, 'Nama': 'Faiza Aqiela Zuma'},
 	{'NIM': 18220085, 'Nama': 'Haje Noorjamani'},
-	{'NIM': 18220005, 'Nama': 'Muhammad Rifqi Riyansah'}
+	{'NIM': 18220005, 'Nama': 'Muhammad Rifqi Riyansah'},
+	{'NIM': 18220063, 'Nama': 'I Nyoman Aditya Ariputra'}
 ]
 
 @app.get("/")
